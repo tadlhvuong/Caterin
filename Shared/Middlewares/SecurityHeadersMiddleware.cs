f02dc3bce5fe924e78e11cc34a5f2826707bb023
tadlhvuong <tadlhvuong@gmail.com>
2026-07-15 15:24:43 +0700

    add source admin, setup menu

--- Shared/Middlewares/SecurityHeadersMiddleware.cs
+++ Shared/Middlewares/SecurityHeadersMiddleware.cs
@@ -14,16 +14,36 @@ namespace Shared.Middlewares
         {
             return app.Use(async (context, next) =>
             {
+                //X-Content-Type-Options: nosniff
+                //Referrer-Policy: strict-origin-when-cross-origin
+                //Permissions-Policy: geolocation=(), microphone=(), camera=()
+                //X-Frame-Options: DENY
+                //Cross-Origin-Opener-Policy: same-origin
+                //Cross-Origin-Resource-Policy: same-origin
+
                 context.Response.Headers["Content-Security-Policy"] =
                    "default-src 'self'; " +
-                   "script-src 'self' 'unsafe-inline' https://unpkg.com https://cdn.jsdelivr.net; " +
+                   "base-uri 'self'; " +
+                   "script-src 'self' 'unsafe-inline' https://unpkg.com https://cdn.jsdelivr.net https://maps.googleapis.com https://maps.gstatic.com; " +
                    "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com; " +
-                   "img-src 'self' data: https:; " +
+                   "img-src 'self'  https://*.googleapis.com https://*.gstatic.com data: https:; " +
                    "font-src 'self' https://fonts.gstatic.com https:; " +
                    "connect-src 'self'; " +
+                   "object-src 'none';" +
                    "frame-ancestors 'none'; " +
-                   "base-uri 'self'; " +
-                   "form-action 'self';";
+                   "form-action 'self';" +
+                   "upgrade-insecure-requests;" +
+                   "block-all-mixed-content;";
+                context.Response.Headers["Permissions-Policy"] = 
+                    "geolocation=(self), " +
+                    "camera=(), " +
+                    "microphone=(), " +
+                    "payment=(), " +
+                    "usb=(self), " +
+                    "serial=(self), " +
+                    "bluetooth=(), " +
+                    "hid=(self), " +
+                    "fullscreen=(self)";
                 await next();
             });
         }
